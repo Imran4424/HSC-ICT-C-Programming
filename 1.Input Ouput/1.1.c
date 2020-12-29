4c42f48241f3d2e9d376463f7351ce108a4d6c14
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-30 00:23:34 +0600

    input output

--- 1.Input Ouput/1.1.c
+++ 1.Input Ouput/1.1.c
@@ -1,10 +1,27 @@
 /**
-	write a program that prints your name
+	Write a program that prints your name
 */
 
-#include <stdio.h>
+
+#include <stdio.h>  // for printf() function
 
 int main()
 {
-    return 0;
+
+	printf("SHAH MD. IMRAN HOSSAIN\n");
+
+	return 0;
 }
+
+/**
+	here, printf is used in c for output stream
+	which is a member of stdio.h header file
+
+	'\n' is an escape sequence which prints a new line
+
+    between the double quotation("") of printf("")
+
+    you can write anything and that will print
+
+    try write yourself a paragraph see how it works
+*/
